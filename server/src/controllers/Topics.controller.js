9c192dd46528fd750a302e1aec85f1c30448c1c2
Andreikardan <timoshenko.andrei_1998@mail.ru>
2024-12-19 19:52:18 +0300

    topicsController and thunks create

--- server/src/controllers/Topics.controller.js
+++ server/src/controllers/Topics.controller.js
@@ -1,14 +1,22 @@
+const TopicService = require("../services/Topic.service");
+const formatResponse = require("../utils/formatResponse");
+
 class TopicsController {
-  static async getTopicsWithQuestion(req,res){
+  static async getTopicsWithQuestion(req, res) {
     try {
-      const topics = await Top
-      
-    } catch ({message}) {
+      const topics = await TopicService.get();
+      if (!topics || topics.length === 0) {
+        return res
+          .status(400)
+          .json(formatResponse(400, "No items in DB", null, "No items in DB"));
+      }
+      res.status(200).json(formatResponse(200, "Success", topics));
+    } catch ({ message }) {
       res
-      .status(500)
-      .json(formatResponse(500, "Internal server error", null, message));
-    console.log(message);
+        .status(500)
+        .json(formatResponse(500, "Internal server error", null, message));
+      console.log(message);
     }
   }
 }
-module.exports = TopicsController
+module.exports = TopicsController;
