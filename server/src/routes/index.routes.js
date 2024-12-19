8fc9f1409aea81eddb5fbfde065521e9cd367ed5
Andreikardan <timoshenko.andrei_1998@mail.ru>
2024-12-19 19:00:12 +0300

    topics service/ get create

--- server/src/routes/index.routes.js
+++ server/src/routes/index.routes.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
 const userRouter = require("./auth.routes");
+const topicsRoutes =require('./topics.routes')
 
 router.use("/auth", userRouter);
-
+router.use('/topics',topicsRoutes)
 
 
 
